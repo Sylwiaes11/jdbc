a4435e16eee34943b09e11afe998fa49604d25fb
s11@kurs.pl <s11@kurs.pl>
2018-06-10 14:23:59 +0200

    operacja dla klasy User

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -3,10 +3,10 @@ package dao;
 import config.Database;
 import model.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class UserDao {
     // save
@@ -17,7 +17,7 @@ public class UserDao {
 
     private Database database = new Database();
 
-    public User save (User user) throws SQLException {
+    public User save(User user) throws SQLException {
         // insert
         // 1. pobierz połączenie
         Connection connection = database.getConnection();
@@ -43,21 +43,82 @@ public class UserDao {
 
     }
 
-    public void update (User user){
+    public void update(User user) throws SQLException {
+
+
+        Connection connection = database.getConnection();
+        String sql = "UPDATE user SET first_name = ?, last_name = ?, email = ? WHERE id=?";
+
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        statement.setString(1, user.getFirstName());
+        statement.setString(2, user.getLastName());
+        statement.setString(3, user.getEmail());
+        statement.setInt(4, user.getId());
+
+        statement.executeUpdate();
+
 
     }
 
-    public void delete (int id){
+    public void delete(int id) throws SQLException {
+        Connection connection = database.getConnection();
+        String sql = "DELETE FROM user WHERE ID=?";
+
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        statement.setInt(1, id);
+
+
+        statement.executeUpdate();
+
 
     }
 
-    public User findById (int id){
-        return null;
+    public User findById(int id) throws SQLException {
+        Connection connection = database.getConnection();
+
+        String sql = "SELECT id, first_name, last_name, email FROM user WHERE id=?";
+
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        ResultSet result = statement.executeQuery();
+        User user = null;
+
+        while (result.next()) {
+            id = result.getInt("id");
+            String firstName = result.getString("first_name");
+            String lastName = result.getString("last_name");
+            String email = result.getString("email");
+            user = new User(id, firstName, lastName, email);
+        }
+        return user;
 
     }
 
-    public List<User> findAll(){
-        return null;
+
+    public List<User> findAll() throws SQLException {
+        Connection connection = database.getConnection();
+
+        String sql = "SELECT id, first_name, last_name, email FROM user";
+
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        ResultSet result = statement.executeQuery();
+        List<User> users = new ArrayList<>();
+
+        while (result.next()) {
+            int id = result.getInt("id");
+            String firstName = result.getString("first_name");
+            String lastName = result.getString("last_name");
+            String email = result.getString("email");
+            users.add(new User(id, firstName, lastName, email));
+
+        }
+
+
+        return users;
+
 
     }
 }
